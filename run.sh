659f535aa0720a85d7083f2d819044b68bb78377
kt <feikong@shouxin168.com>
2023-08-25 03:56:10 +0000

    test

--- run.sh
+++ run.sh
@@ -4,7 +4,14 @@ cd /home/test
 # 使环境变量生效
 source /root/.bash_profile
 # kill
-ps -ef|grep java|grep test.jar|cut -c9-14|xargs kill -9
+echo ">>>>>> 停止服务"
+pid=$(ps -ef | grep test.jar | grep -v grep | awk '{print $2}')
+if [ "$pid" == "" ]; then
+    echo ">>>>>> 服务未启动"
+else
+    kill -9 $pid
+    echo ">>>>>> 服务停止完成"
+fi
 # 运行项目
 nohup /usr/java/jdk1.8.0_181-cloudera/bin/java -jar test.jar >/home/test/cicd.log 2>&1 &
 echo "启动完成"
