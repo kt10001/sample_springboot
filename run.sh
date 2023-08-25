9349f9b0bdf7659c7c766597f6fa8155a78e17ab
kt <feikong@shouxin168.com>
2023-08-25 03:39:28 +0000

    test

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 cd /home/test
 # 杀死之前的项目进程。这里的cut -c9-14是截取进程id，不一定都是-c9-14
 # 可以先运行ps -ef|grep java|grep GitHub_Action-0.0.1-SNAPSHOT.jar看一下进程id是第几位到第几位
-ps -ef | grep test.jar | grep -v grep | awk '{print $2}')|xargs kill -9
+ps -ef | grep test.jar | grep -v grep | awk '{print $2}' | xargs kill -9
 echo ">>>>>> 停止服务"
 # 停5秒
 sleep 5s
