a98b314807a15b3595b2b98cd46cd2360e0dc252
kt <feikong@shouxin168.com>
2023-08-25 03:37:37 +0000

    test

--- run.sh
+++ run.sh
@@ -3,15 +3,8 @@
 cd /home/test
 # 杀死之前的项目进程。这里的cut -c9-14是截取进程id，不一定都是-c9-14
 # 可以先运行ps -ef|grep java|grep GitHub_Action-0.0.1-SNAPSHOT.jar看一下进程id是第几位到第几位
-# ps -ef|grep java|grep test.jar|cut -c9-14|xargs kill -9
+ps -ef | grep test.jar | grep -v grep | awk '{print $2}')|xargs kill -9
 echo ">>>>>> 停止服务"
-pid=$(ps -ef | grep test.jar | grep -v grep | grep -v run.sh | awk '{print $2}')
-if [ "$pid" == "" ]; then
-    echo ">>>>>> 服务未启动"
-else
-    kill -9 $pid
-    echo ">>>>>> 服务停止完成"
-fi
 # 停5秒
 sleep 5s
 # 使环境变量生效
