5314488147b2e40da7175e2494a486c76b5c8b9a
kt <feikong@shouxin168.com>
2023-08-25 03:52:28 +0000

    test

--- run.sh
+++ run.sh
@@ -4,5 +4,5 @@ cd /home/test
 # 使环境变量生效
 source /root/.bash_profile
 # 运行项目
-nohup /usr/java/jdk1.8.0_181-cloudera/bin/java -jar test.jar &
+nohup /usr/java/jdk1.8.0_181-cloudera/bin/java -jar test.jar >/home/test/cicd.log 2>&1 &
 echo "启动完成"
