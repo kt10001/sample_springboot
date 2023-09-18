860528a1b7e50314261d816f600681d0f43efdaa
fiekong <feikong@shouxin168.com>
2023-09-18 15:11:35 +0800

    mybatis

---
+++ src/main/java/com/test/fk/config/MyBatisConfig.java
@@ -0,0 +1,13 @@
+package com.test.fk.config;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * MyBatis配置类
+ * Created by macro on 2019/4/8.
+ */
+@Configuration
+@MapperScan("com.test.fk.mbg.mapper")
+public class MyBatisConfig {
+}
